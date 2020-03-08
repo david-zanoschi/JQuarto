5589a03b556e7f67b9000fd95257f0c921a3e2ed
25768329 <zanoschi.david@yahoo.com>
2020-03-08 18:06:02 +0200

    player class

---
+++ JQuarto/src/quarto/ai/Node.java
@@ -0,0 +1,17 @@
+package quarto.ai;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node<T>
+{
+	public T data;
+	public Node<T> parent;
+	public List<Node<T>> children;
+	
+	public Node(T dataParam)
+	{
+		data = dataParam;
+		children = new ArrayList<Node<T>>();
+	}
+}
---
+++ JQuarto/src/quarto/engine/player/Player.java
@@ -0,0 +1,16 @@
+package quarto.engine.player;
+
+import quarto.engine.board.Board;
+
+public class Player 
+{
+	void choosePiece(Board board)
+	{
+		// simuleaza un click  pe piesa
+	}
+	
+	void placePiece(Board board) 
+	{
+		// simuleaza un click pe tabla
+	}
+}
